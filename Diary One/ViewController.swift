a7d53f9077045032e8d2fedffc9f8221622d662b
mindelicious <[mailprogramisty@gmail.com]>
2019-01-09 22:11:20 +0100

    Initial Commit

---
+++ Diary One/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diary One
+//
+//  Created by Matt on 09/01/2019.
+//  Copyright © 2019 mindelicious. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
